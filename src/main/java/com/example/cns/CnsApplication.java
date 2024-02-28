aa037074ddac2e3106e2525d5e1fd6192c7f0570
jueun oh <jueun1025@naver.com>
2024-02-28 23:03:43 +0900

    chore : 프로젝트 생성

---
+++ src/main/java/com/example/cns/CnsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cns/CnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
