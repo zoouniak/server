9c82bbecefcd8659af7bcf137471c48a56b6c46d
jueun oh <jueun1025@naver.com>
2024-03-30 15:21:46 +0900

    fix : object mapper 직렬화/역직렬화 문제 해결

--- src/main/java/com/example/cns/common/security/config/SecurityConfig.java
+++ src/main/java/com/example/cns/common/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.cns.common.security.config;
 import com.example.cns.common.security.exception.handler.JwtAccessDeniedHandler;
 import com.example.cns.common.security.exception.handler.JwtAuthenticationEntryPoint;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,7 +36,9 @@ public class SecurityConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 
     @Bean
