76f31a0a3fe436db0970b46a0fad585027455cf9
jueun oh <jueun1025@naver.com>
2024-04-15 22:22:30 +0900

    feat : 오류 코드 명세

--- src/main/java/com/example/cns/common/exception/ExceptionCode.java
+++ src/main/java/com/example/cns/common/exception/ExceptionCode.java
@@ -9,27 +9,23 @@ import static org.springframework.http.HttpStatus.*;
 @RequiredArgsConstructor
 @Getter
 public enum ExceptionCode {
-    MEMBER_NOT_FOUND(6001, HttpStatus.BAD_REQUEST, "사용자가 존재하지 않습니다."),
-    INVALID_EMAIL_FORMAT(3001, BAD_REQUEST, "회사 이메일과 일치하지 않습니다."),
-    EMPTY_ACCESS_TOKEN(6001, BAD_REQUEST, "토큰이 비어있습니다."),
-    MALFORMED_TOKEN(6001, UNAUTHORIZED, "손상된 토큰입니다."),
-    FAIL_TOKEN(6001, UNAUTHORIZED, "검증에 실패한 토큰입니다."),
-    UNSUPPORTED_TOKEN(6001, UNAUTHORIZED, "지원하지 않는 토큰입니다."),
-    INVALID_TOKEN(6001, BAD_REQUEST, "유효하지 않은 토큰입니다."),
-    INVALID_ROLE(6001, BAD_REQUEST, "유효하지 않은 사용자입니다."),
-    UNAUTHORIZED_USER(4001, UNAUTHORIZED, "인증되지 않은 사용자입니다."),
-    FORBIDDEN_REQUEST(4002, FORBIDDEN, "허가되지 않은 요청입니다."),
-
-    EXPIRED_TOKEN(6001, UNAUTHORIZED, "만료된 토큰입니다."),
-
-    DUPLICATE_EMAIL_EXISTS(6001, BAD_REQUEST, "이미 사용 중인 이메일입니다."),
-    DUPLICATE_USERNAME_EXISTS(6001, BAD_REQUEST, "이미 사용 중인 아이디입니다."),
-
-    INVALID_PASSWORD(6001, BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
-    INCORRECT_AUTHENTICATION_NUMBER(6001, UNAUTHORIZED, "인증번호가 일치하지 않습니다"),
-    INVALID_EMAIL(6001, BAD_REQUEST, "인증에 실패하였습니다."),
-    COMPANY_NOT_EXIST(3001, BAD_REQUEST, "회사가 존재하지 않습니다."),
-    INCORRECT_INFO(6002, BAD_REQUEST, "정보 불일치");
+    // 인증 1000번대
+    EMPTY_ACCESS_TOKEN(1001, BAD_REQUEST, "토큰이 비어있습니다."),
+    EXPIRED_TOKEN(1002, UNAUTHORIZED, "만료된 토큰입니다."),
+    INVALID_TOKEN(1003, BAD_REQUEST, "유효하지 않은 토큰입니다."),
+    INVALID_ROLE(1004, BAD_REQUEST, "유효하지 않은 사용자입니다."),
+    UNAUTHORIZED_USER(1005, UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+    FORBIDDEN_REQUEST(1006, FORBIDDEN, "허가되지 않은 요청입니다."),
+    INVALID_EMAIL_FORMAT(1007, BAD_REQUEST, "회사 이메일과 일치하지 않습니다."),
+    DUPLICATE_EMAIL_EXISTS(1008, BAD_REQUEST, "이미 사용 중인 이메일입니다."),
+    DUPLICATE_USERNAME_EXISTS(1009, BAD_REQUEST, "이미 사용 중인 아이디입니다."),
+    INVALID_PASSWORD(1010, BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    INCORRECT_AUTHENTICATION_NUMBER(1011, UNAUTHORIZED, "인증번호가 일치하지 않습니다."),
+    INVALID_EMAIL(1012, BAD_REQUEST, "인증에 실패하였습니다."),
+    INCORRECT_INFO(1013, BAD_REQUEST, "회원정보와 일치하지 않습니다."),
+    // 회원 2000번대
+    MEMBER_NOT_FOUND(2001, BAD_REQUEST, "사용자가 존재하지 않습니다."),
+    COMPANY_NOT_EXIST(2002, BAD_REQUEST, "회사가 존재하지 않습니다.");
 
     private final int errorCode;
     private final HttpStatus httpStatus;
