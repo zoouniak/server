25608cd1946a79e3aef054020a994f38ec9014b8
jueun oh <jueun1025@naver.com>
2024-05-13 00:29:57 +0900

    chore : 파일 패키지 위치 수정

--- src/main/java/com/example/cns/chat/dto/response/LoginResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.cns.chat.dto.response;
-
-import com.example.cns.auth.dto.response.AuthTokens;
-
-public record LoginResponse(
-        AuthTokens authTokens,
-        Long memberId
-) {
-}
