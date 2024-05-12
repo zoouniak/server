05f3e94451d9fb48fb67258c3985d096c21d171a
jueun oh <jueun1025@naver.com>
2024-05-12 22:05:35 +0900

    fix : 메시지 타입 변수 추가

--- src/main/java/com/example/cns/chat/dto/request/ImageMessageFormat.java
+++ src/main/java/com/example/cns/chat/dto/request/ImageMessageFormat.java
@@ -1,12 +1,6 @@
 package com.example.cns.chat.dto.request;
 
-import com.example.cns.chat.domain.Chat;
-import com.example.cns.chat.domain.ChatRoom;
-import com.example.cns.chat.type.MessageType;
-import com.example.cns.member.domain.Member;
 import lombok.NonNull;
-
-import java.time.LocalDateTime;
 
 public record ImageMessageFormat(
         @NonNull
@@ -19,19 +13,12 @@ public record ImageMessageFormat(
         String extension,
 
         @NonNull
+        String messageType,
+
+        @NonNull
         Long roomId,
 
         @NonNull
         Long memberId
 ) {
-    public Chat toChatEntity(ChatRoom chatRoom, Member sender, LocalDateTime now) {
-        return Chat.builder()
-                .from(sender)
-                .messageType(MessageType.IMAGE)
-                .createdAt(now)
-                .subjectId(null)
-                .content("")
-                .chatRoom(chatRoom)
-                .build();
-    }
 }
