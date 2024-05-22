7a9868ba217f114c2780b8f4abe6a653bdee4117
jueun oh <jueun1025@naver.com>
2024-05-23 01:11:49 +0900

    fix : 쿼리 오류 수정

--- src/main/java/com/example/cns/project/plan/domain/repository/PlanRepository.java
+++ src/main/java/com/example/cns/project/plan/domain/repository/PlanRepository.java
@@ -2,6 +2,8 @@ package com.example.cns.project.plan.domain.repository;
 
 import com.example.cns.project.plan.domain.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +11,6 @@ import java.util.List;
 @Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
-    List<Plan> getAllByProject(Long projectId);
+    @Query("select p from Plan p where p.project.id=:projectId")
+    List<Plan> getAllByProject(@Param("projectId") Long projectId);
 }
