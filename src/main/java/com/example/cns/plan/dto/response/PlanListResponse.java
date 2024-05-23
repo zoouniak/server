064109f82be0493da057d8b56e2960f5974ed256
jueun oh <jueun1025@naver.com>
2024-05-23 14:56:12 +0900

    fix : 날짜 포맷 설정

--- src/main/java/com/example/cns/plan/dto/response/PlanListResponse.java
+++ src/main/java/com/example/cns/plan/dto/response/PlanListResponse.java
@@ -1,10 +1,16 @@
 package com.example.cns.plan.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public record PlanListResponse(
         String planName,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
         LocalDateTime startedAt,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
         LocalDateTime endedAt
 ) {
 
