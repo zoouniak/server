84599576a2fb8079ba6dae512fd142b0fa09a802
jueun oh <jueun1025@naver.com>
2024-05-23 14:30:36 +0900

    fix : 날짜 포맷 설정

--- src/main/java/com/example/cns/project/dto/request/PlanCreateRequest.java
+++ src/main/java/com/example/cns/project/dto/request/PlanCreateRequest.java
@@ -2,6 +2,7 @@ package com.example.cns.project.dto.request;
 
 import com.example.cns.project.domain.Project;
 import com.example.cns.project.plan.domain.Plan;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -15,9 +16,11 @@ public record PlanCreateRequest(
         String planName,
 
         @NotBlank
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
         LocalDateTime startedAt,
 
         @Future
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
         LocalDateTime endedAt,
 
         @NotBlank
--- src/main/java/com/example/cns/project/dto/response/PlanDetailResponse.java
+++ src/main/java/com/example/cns/project/dto/response/PlanDetailResponse.java
@@ -1,13 +1,17 @@
 package com.example.cns.project.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record PlanDetailResponse(
         String planName,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
         LocalDateTime startedAt,
 
-
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
         LocalDateTime endedAt,
 
         String content,
