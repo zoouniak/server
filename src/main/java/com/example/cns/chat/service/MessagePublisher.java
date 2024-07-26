e7989b20bfdc9dbbcfba4082dcf4120d3ff6a2fb
jueun oh <jueun1025@naver.com>
2024-07-26 19:46:59 +0900

    fix : 메시지 브로커 수정

--- src/main/java/com/example/cns/chat/service/MessagePublisher.java
+++ src/main/java/com/example/cns/chat/service/MessagePublisher.java
@@ -1,7 +1,12 @@
 package com.example.cns.chat.service;
 
 import com.example.cns.chat.dto.response.ChatResponse;
+import com.example.cns.chat.dto.response.UnRead;
+
+import java.util.List;
 
 public interface MessagePublisher {
     void publishMessage(Long roomId, ChatResponse chat);
+
+    void publishUnRead(Long roomId, List<UnRead> unReadList);
 }
--- src/main/java/com/example/cns/chat/service/SimpMessagePublisher.java
+++ src/main/java/com/example/cns/chat/service/SimpMessagePublisher.java
@@ -1,25 +1,36 @@
-package com.example.cns.chat.service.redisMessageBroker;
+package com.example.cns.chat.service;
 
 import com.example.cns.chat.dto.response.ChatResponse;
-import com.example.cns.chat.service.MessagePublisher;
+import com.example.cns.chat.dto.response.UnRead;
 import com.example.cns.common.exception.ChatException;
 import com.example.cns.common.exception.ExceptionCode;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SimpMessagePublisher implements MessagePublisher {
-    private final SimpMessageSendingOperations messageOp;
+    private final SimpMessagingTemplate messageOp;
     private final ObjectMapper mapper;
 
     @Override
     public void publishMessage(Long roomId, ChatResponse chat) {
         try {
             messageOp.convertAndSend("/sub/chat-room/" + roomId, mapper.writeValueAsString(chat));
+        } catch (JsonProcessingException e) {
+            throw new ChatException(ExceptionCode.FAIL_SEND_INFOMSG);
+        }
+    }
+
+    @Override
+    public void publishUnRead(Long roomId, List<UnRead> unReadList) {
+        try {
+            messageOp.convertAndSend("/unread/" + roomId, mapper.writeValueAsString(unReadList));
         } catch (JsonProcessingException e) {
             throw new ChatException(ExceptionCode.FAIL_SEND_INFOMSG);
         }
