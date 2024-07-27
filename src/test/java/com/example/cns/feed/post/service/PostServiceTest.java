a4fd44564098e3cdb6cd900ccb41419140039322
Evon00 <dbwogur36@gmail.com>
2024-07-27 18:25:52 +0900

    refactor : 검증 로직 추가에 따른 테스트 코드 수정

--- src/test/java/com/example/cns/feed/post/service/PostServiceTest.java
+++ src/test/java/com/example/cns/feed/post/service/PostServiceTest.java
@@ -14,7 +14,9 @@ import com.example.cns.feed.post.domain.repository.PostListRepository;
 import com.example.cns.feed.post.domain.repository.PostRepository;
 import com.example.cns.feed.post.dto.request.PostRequest;
 import com.example.cns.feed.post.dto.response.FileResponse;
+import com.example.cns.hashtag.domain.HashTag;
 import com.example.cns.hashtag.domain.HashTagPost;
+import com.example.cns.hashtag.domain.HashTagPostId;
 import com.example.cns.hashtag.domain.repository.HashTagPostRepository;
 import com.example.cns.hashtag.domain.repository.HashTagRepository;
 import com.example.cns.hashtag.service.HashTagService;
@@ -29,7 +31,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.parameters.P;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDate;
@@ -53,8 +57,6 @@ class PostServiceTest {
     @Mock
     private MentionService mentionService;
     @Mock
-    private S3Service s3Service;
-    @Mock
     private CommentService commentService;
     @Mock
     private PostRepository postRepository;
@@ -65,13 +67,7 @@ class PostServiceTest {
     @Mock
     private HashTagPostRepository hashTagPostRepository;
     @Mock
-    private PostLikeRepository postLikeRepository;
-    @Mock
-    private PostListRepository postListRepository;
-    @Mock
     private MentionRepository mentionRepository;
-    @Mock
-    private HashTagRepository hashTagRepository;
 
     @Test
     @DisplayName("해시태그, 언급, 파일이 없는 경우에도 글 생성이 가능해야 한다.")
@@ -87,7 +83,36 @@ class PostServiceTest {
         PostRequest postRequest = craetePostRequest(content,hashTag,mention,postFileList);
         Post post = postRequest.toEntity(member);
         ReflectionTestUtils.setField(post,"id",1L);
-        ReflectionTestUtils.setField(post, "member", member);
+
+        //when
+        when(memberRepository.findById(anyLong())).thenReturn(Optional.of(member));
+        when(postRepository.save(any(Post.class))).thenReturn(post);
+
+        Long savedPostId = postService.savePost(member.getId(),postRequest);
+
+        //then
+        assertEquals(post.getId(),savedPostId);
+
+        verify(postRepository,times(1)).save(any(Post.class));
+        verify(mentionService,times(0)).savePostMention(anyLong(),anyList());
+        verify(hashTagService,times(0)).createHashTag(anyLong(),anyList());
+        verify(postFileRepository,times(postFileList.size())).save(any(PostFile.class));
+    }
+
+    @Test
+    @DisplayName("해시태그가 존재하며, 언급, 파일이 없는 경우에도 글 생성이 가능해야 한다.")
+    void create_feed_success_with_hashtag_no_mention_no_fileList(){
+        //given
+        Member member = createMember();
+
+        String content = "test content\n#test #test1";
+        List<String> hashTag = List.of("test", "test1");
+        List<String> mention = new ArrayList<>();
+        List<FileResponse> postFileList = new ArrayList<>();
+
+        PostRequest postRequest = craetePostRequest(content,hashTag,mention,postFileList);
+        Post post = postRequest.toEntity(member);
+        ReflectionTestUtils.setField(post,"id",1L);
 
         //when
         when(memberRepository.findById(any())).thenReturn(Optional.of(member));
@@ -97,13 +122,72 @@ class PostServiceTest {
 
         //then
         assertEquals(post.getId(),savedPostId);
-        assertEquals(0, hashTagPostRepository.findAllByPostId(savedPostId).size());
-        assertEquals(0,mentionRepository.findMentionsBySubjectId(List.of(savedPostId),MentionType.FEED).size());
 
         verify(postRepository,times(1)).save(any(Post.class));
-        verify(mentionService,times(1)).savePostMention(savedPostId,mention);
-        verify(hashTagService,times(1)).createHashTag(savedPostId,hashTag);
-        verify(postFileRepository,times(0)).save(any(PostFile.class));
+        verify(mentionService,times(0)).savePostMention(anyLong(),anyList());
+        verify(hashTagService,times(1)).createHashTag(anyLong(),anyList());
+        verify(postFileRepository,times(postFileList.size())).save(any(PostFile.class));
+    }
+
+    @Test
+    @DisplayName("언급이 존재하며, 해시태그, 파일이 없는 경우에도 글 생성이 가능해야 한다.")
+    void create_feed_success_with_no_hashtag_mention_no_fileList(){
+        //given
+        Member member = createMember();
+
+        String content = "test content\n@testMember";
+        List<String> hashTag = new ArrayList<>();
+        List<String> mention = List.of("@testMember");
+        List<FileResponse> postFileList = new ArrayList<>();
+
+        PostRequest postRequest = craetePostRequest(content,hashTag,mention,postFileList);
+        Post post = postRequest.toEntity(member);
+        ReflectionTestUtils.setField(post,"id",1L);
+
+        //when
+        when(memberRepository.findById(any())).thenReturn(Optional.of(member));
+        when(postRepository.save(any(Post.class))).thenReturn(post);
+
+        Long savedPostId = postService.savePost(member.getId(),postRequest);
+
+        //then
+        assertEquals(post.getId(),savedPostId);
+
+        verify(postRepository,times(1)).save(any(Post.class));
+        verify(mentionService,times(1)).savePostMention(anyLong(),anyList());
+        verify(hashTagService,times(0)).createHashTag(anyLong(),anyList());
+        verify(postFileRepository,times(postFileList.size())).save(any(PostFile.class));
+    }
+
+    @Test
+    @DisplayName("해시태그, 언급이 존재하지 않으며, 파일이 존재할때 글 생성이 가능해야 한다.")
+    void create_feed_success_with_no_hashtag_no_mention_fileList(){
+        //given
+        Member member = createMember();
+
+        String content = "test content";
+        List<String> hashTag = new ArrayList<>();
+        List<String> mention = new ArrayList<>();
+        List<FileResponse> postFileList = List.of(new FileResponse("uploadFileName1","/file1.png", FileType.PNG), new FileResponse("uploadFileName2","/file2.png",FileType.PNG));
+
+        PostRequest postRequest = craetePostRequest(content,hashTag,mention,postFileList);
+        Post post = postRequest.toEntity(member);
+        ReflectionTestUtils.setField(post,"id",1L);
+
+        //when
+        when(memberRepository.findById(anyLong())).thenReturn(Optional.of(member));
+        when(postRepository.save(any(Post.class))).thenReturn(post);
+        when(postRepository.findById(anyLong())).thenReturn(Optional.of(post));
+
+        Long savedPostId = postService.savePost(member.getId(),postRequest);
+
+        //then
+        assertEquals(post.getId(),savedPostId);
+
+        verify(postRepository,times(1)).save(any(Post.class));
+        verify(mentionService,times(0)).savePostMention(anyLong(),anyList());
+        verify(hashTagService,times(0)).createHashTag(anyLong(),anyList());
+        verify(postFileRepository,times(postFileList.size())).save(any(PostFile.class));
     }
 
     @Test
@@ -120,7 +204,6 @@ class PostServiceTest {
         PostRequest postRequest = craetePostRequest(content,hashTag,mention,postFileList);
         Post post = postRequest.toEntity(member);
         ReflectionTestUtils.setField(post,"id",1L);
-        ReflectionTestUtils.setField(post, "member", member);
 
         //when
         when(memberRepository.findById(any())).thenReturn(Optional.of(member));
@@ -131,26 +214,74 @@ class PostServiceTest {
         //then
         assertEquals(post.getId(),savedPostId);
 
-        List<HashTagPost> savedHashTagPost = hashTagPostRepository.findAllByPostId(savedPostId);
-        savedHashTagPost.forEach(
-                hashTagPost -> {
-                    assertEquals(savedPostId,hashTagPost.getId().getPost().longValue());
-                }
-        );
-
-        List<Object[]> savedMentions = mentionRepository.findMentionsBySubjectId(List.of(savedPostId), MentionType.FEED);
-        savedMentions.forEach(
-                mentionObject -> {
-                      assertEquals(savedPostId,mentionObject[0]);
-                }
-        );
-
         verify(postRepository,times(1)).save(any(Post.class));
-        verify(mentionService,times(1)).savePostMention(eq(savedPostId),eq(mention));
-        verify(hashTagService,times(1)).createHashTag(eq(savedPostId),eq(hashTag));
-        verify(postFileRepository,times(0)).save(any(PostFile.class));
+        verify(mentionService,times(1)).savePostMention(anyLong(),anyList());
+        verify(hashTagService,times(1)).createHashTag(anyLong(),anyList());
+        verify(postFileRepository,times(postFileList.size())).save(any(PostFile.class));
     }
 
+    @Test
+    @DisplayName("해시태그는 존재하지 않으며, 언급, 파일이 있는 경우에도 글 생성이 가능해야 한다.")
+    void create_feed_success_with_no_hashtag_mention_fileList(){
+        //given
+        Member member = createMember();
+
+        String content = "test content\n@testMember";
+        List<String> hashTag = new ArrayList<>();
+        List<String> mention = List.of("@testMember");
+        List<FileResponse> postFileList = List.of(new FileResponse("uploadFileName1","/file1.png", FileType.PNG), new FileResponse("uploadFileName2","/file2.png",FileType.PNG));
+
+        PostRequest postRequest = craetePostRequest(content,hashTag,mention,postFileList);
+        Post post = postRequest.toEntity(member);
+        ReflectionTestUtils.setField(post,"id",1L);
+
+        //when
+        when(memberRepository.findById(anyLong())).thenReturn(Optional.of(member));
+        when(postRepository.save(any(Post.class))).thenReturn(post);
+        when(postRepository.findById(anyLong())).thenReturn(Optional.of(post));
+
+        Long savedPostId = postService.savePost(member.getId(),postRequest);
+
+        //then
+        assertEquals(post.getId(),savedPostId);
+
+        verify(postRepository,times(1)).save(any(Post.class));
+        verify(mentionService,times(1)).savePostMention(anyLong(),anyList());
+        verify(hashTagService,times(0)).createHashTag(anyLong(),anyList());
+        verify(postFileRepository,times(postFileList.size())).save(any(PostFile.class));
+    }
+
+    @Test
+    @DisplayName("해시태그, 파일이 존재하며 언급이 없을때 글 생성이 가능해야 한다.")
+    void create_feed_success_with_hashtag_no_mention_fileList(){
+        //given
+        Member member = createMember();
+
+        String content = "test content\n#test #test1";
+        List<String> hashTag = List.of("test","test1");
+        List<String> mention = new ArrayList<>();
+        List<FileResponse> postFileList = List.of(new FileResponse("uploadFileName","/file1.png", FileType.PNG));
+
+        PostRequest postRequest = craetePostRequest(content,hashTag,mention,postFileList);
+        Post post = postRequest.toEntity(member);
+        ReflectionTestUtils.setField(post,"id",1L);
+
+        //when
+        when(memberRepository.findById(anyLong())).thenReturn(Optional.of(member));
+        when(postRepository.save(any(Post.class))).thenReturn(post);
+        when(postRepository.findById(anyLong())).thenReturn(Optional.of(post));
+
+        //hashTag 설정
+        Long savedPostId = postService.savePost(member.getId(),postRequest);
+
+        //then
+        assertEquals(post.getId(),savedPostId);
+
+        verify(postRepository,times(1)).save(any(Post.class));
+        verify(mentionService,times(0)).savePostMention(anyLong(),anyList());
+        verify(hashTagService,times(1)).createHashTag(anyLong(),anyList());
+        verify(postFileRepository,times(postFileList.size())).save(any(PostFile.class));
+    }
     @Test
     @DisplayName("해시태그, 언급, 파일이 존재할때 글 생성이 가능해야 한다.")
     void create_feed_success_with_hashtag_mention_fileList(){
@@ -167,8 +298,6 @@ class PostServiceTest {
         ReflectionTestUtils.setField(post,"id",1L);
         ReflectionTestUtils.setField(post, "member", member);
 
-        List<PostFile> postFiles = createPostFile(post,postFileList);
-
         //when
         when(memberRepository.findById(anyLong())).thenReturn(Optional.of(member));
         when(postRepository.save(any(Post.class))).thenReturn(post);
@@ -179,31 +308,38 @@ class PostServiceTest {
         //then
         assertEquals(post.getId(),savedPostId);
 
+        verify(postRepository,times(1)).save(any(Post.class));
+        verify(mentionService,times(1)).savePostMention(anyLong(),anyList());
+        verify(hashTagService,times(1)).createHashTag(anyLong(),anyList());
+        verify(postFileRepository,times(postFileList.size())).save(any(PostFile.class));
+    }
+
+    private void verifySavedPost(Post post, Long savedPostId, List<String> mention, List<String> hashTag, List<PostFile> postFiles){
+
+        assertEquals(post.getId(),savedPostId);
+
         List<HashTagPost> savedHashTagPost = hashTagPostRepository.findAllByPostId(savedPostId);
-        savedHashTagPost.forEach(
-                hashTagPost -> {
-                    assertEquals(savedPostId,hashTagPost.getId().getPost().longValue());
-                }
-        );
+        assertEquals(hashTag.size(),savedHashTagPost.size());
+        for(HashTagPost hashTagPost : savedHashTagPost){
+            assertEquals(savedPostId,hashTagPost.getId().getPost().longValue());
+        }
 
         List<Object[]> savedMentions = mentionRepository.findMentionsBySubjectId(List.of(savedPostId), MentionType.FEED);
-        savedMentions.forEach(
-                mentionObject -> {
-                    assertEquals(savedPostId,mentionObject[0]);
-                }
-        );
+        assertEquals(mention.size(),savedMentions.size());
+        for(Object[] mentionObject : savedMentions){
+            assertEquals(savedPostId,mentionObject[0]);
+        }
 
         List<PostFile> savedPostFiles = postFileRepository.findAllByPostId(savedPostId);
-        savedPostFiles.forEach(
-                postFile -> {
-                    assertEquals(savedPostId,postFile.getPost().getId());
-                }
-        );
+        assertEquals(postFiles.size(),savedPostFiles.size());
+        for(PostFile postFile : savedPostFiles){
+            assertEquals(savedPostId,postFile.getPost().getId());
+        }
 
         verify(postRepository,times(1)).save(any(Post.class));
         verify(mentionService,times(1)).savePostMention(eq(savedPostId),eq(mention));
         verify(hashTagService,times(1)).createHashTag(eq(savedPostId),eq(hashTag));
-        verify(postFileRepository,times(postFiles.size())).save(any(PostFile.class));
+        verify(postFileRepository,times(savedPostFiles.size())).save(any(PostFile.class));
     }
 
     @Test
@@ -233,9 +369,9 @@ class PostServiceTest {
 
         //then
         verify(commentService,times(post.getComments().size())).deleteComment(anyLong(),any(CommentDeleteRequest.class));
-        verify(hashTagService,times(1)).deleteHashTag(post.getId());
-        verify(mentionService,times(1)).deletePostMention(post.getId());
-        verify(postRepository,times(1)).deleteById(post.getId());
+        verify(hashTagService,times(1)).deleteHashTag(eq(post.getId()));
+        verify(mentionService,times(1)).deletePostMention(eq(post.getId()));
+        verify(postRepository,times(1)).deleteById(eq(post.getId()));
 
     }
 
@@ -272,22 +408,4 @@ class PostServiceTest {
     private static PostRequest craetePostRequest(String content, List<String> hashtag, List<String> mention, List<FileResponse> postFileList){
         return new PostRequest(content,hashtag,mention,true,postFileList);
     }
-
-    private static List<PostFile> createPostFile(Post post, List<FileResponse> fileResponses){
-        List<PostFile> postFiles = new ArrayList<>();
-        fileResponses.forEach(
-                fileResponse -> {
-                    PostFile postFile = PostFile.builder()
-                            .post(post)
-                            .fileName(fileResponse.uploadFileName())
-                            .url(fileResponse.uploadFileURL())
-                            .fileType(fileResponse.fileType())
-                            .createdAt(LocalDateTime.now())
-                            .build();
-                    postFiles.add(postFile);
-                }
-        );
-        return postFiles;
-    }
-
 }
